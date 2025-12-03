45856eb55d8a89b4f6ea104c9a98f6d8d93f963d
tymek7707 <trymek7707@gmail.com>
2025-12-03 23:00:43 +0100

    added base templete and homepage

--- footballapp/core/views.py
+++ footballapp/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class HomePageView(View):
+    def get(self, request):
+        return render(request, 'core/home.html')
+class MatchlistView(View):
+    def get(self, request):
+        return render(request, 'core/matchlist.html')
