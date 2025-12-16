4fccac0add90fa91c3c637a709ba8beda1a2d7e8
tymek7707 <trymek7707@gmail.com>
2025-12-16 08:36:26 +0100

    added views , html and styling

--- footballapp/core/urls.py
+++ footballapp/core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from . import views
+from .views import HomePageView, MatchlistView, TeamListView, TeamDetailView, MatchDetailView, LeagueTableView
+
+app_name = 'core'
 
 urlpatterns = [
-    path('' , views.HomePageView.as_view() , name='HomePage'),
-    path('matches/' , views.MatchlistView.as_view() , name='Matchlist'),
+    path('', HomePageView.as_view(), name='home'),
+    path('matches/', MatchlistView.as_view(), name='matchlist'),
+    path('teams/', TeamListView.as_view(), name='team_list'),
+    path('teams/<str:team_id>/', TeamDetailView.as_view(), name='team_detail'),
+    path('matches/<str:match_id>/', MatchDetailView.as_view(), name='match_detail'),
+    path('table/', LeagueTableView.as_view(), name='league_table'),
 ]
--- footballapp/footballapp/settings.py
+++ footballapp/footballapp/settings.py
@@ -115,11 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    BASE_DIR / "core" / "static",  # Dodaj ścieżkę do static w aplikacji core
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
